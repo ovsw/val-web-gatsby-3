25c8c332255fd10f9e0f2f2bd6e3e667cb4a56b2
Ovi Savescu <ovi@ovswebsites.com>
2021-08-17 10:56:01 +0300

    fix: replace old gatsby image with gatsby-image-plugin

--- src/components/serializers/Figure.js
+++ src/components/serializers/Figure.js
@@ -1,33 +1,52 @@
-import React from 'react'
-import Img from 'gatsby-image'
-import {getFluidGatsbyImage} from 'gatsby-source-sanity'
-import clientConfig from '../../../client-config'
+import React from "react";
+// import Img from 'gatsby-image'
+// import { getFluidGatsbyImage } from "gatsby-source-sanity";
 
-export default ({node}) => {
+import { GatsbyImage } from "gatsby-plugin-image";
+import { getGatsbyImageData } from "gatsby-source-sanity";
+import clientConfig from "../../../client-config";
+
+export default ({ node }) => {
   // console.log(node.asset.metadata.dimensions.width)
-  let fluidProps = getFluidGatsbyImage(
-    node.asset._id,
-    {maxWidth: 730},
-    clientConfig.sanity
-  )
 
-  let imageClassName = 'contentImage'
+  // let fluidProps = getFluidGatsbyImage(
+  //   node.asset._id,
+  //   {maxWidth: 730},
+  //   clientConfig.sanity
+  // )
+
+  const gatsbyImageData = getGatsbyImageData(
+    node,
+    { width: 730, fit: "crop", height: 600 },
+    clientConfig.sanity
+  );
+
+  let imageClassName = "contentImage";
 
   if (node.asset.metadata.dimensions.width < 700) {
-    fluidProps = getFluidGatsbyImage(
-      node.asset._id,
-      {maxWidth: 400},
-      clientConfig.sanity
-    )
-    imageClassName = imageClassName + ' small'
+    fluidProps = getFluidGatsbyImage(node.asset._id, { maxWidth: 400 }, clientConfig.sanity);
+    imageClassName = imageClassName + " small";
   }
 
-  if (node.asset.metadata.dimensions.aspectRatio > 1) { imageClassName = imageClassName + ' landscape' } else { imageClassName = imageClassName + ' portrait' }
+  if (node.asset.metadata.dimensions.aspectRatio > 1) {
+    imageClassName = imageClassName + " landscape";
+  } else {
+    imageClassName = imageClassName + " portrait";
+  }
 
   return (
-    <figure className={imageClassName}>
-      <Img fluid={fluidProps} alt={node.alt} />
-      <figcaption>{node.caption}</figcaption>
-    </figure>
-  )
-}
+    <>
+      {node.caption && (
+        <figure className={imageClassName}>
+          {/* <Img fluid={fluidProps} alt={node.alt} /> */}
+          <GatsbyImage image={gatsbyImageData} className="contentImage landscape2" alt={alt} />
+          <figcaption>{node.caption}</figcaption>
+        </figure>
+      )}
+
+      {!node.caption && (
+        <GatsbyImage image={gatsbyImageData} className="contentImage landscape2" alt={alt} />
+      )}
+    </>
+  );
+};

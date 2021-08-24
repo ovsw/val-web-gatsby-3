9ec2ca67d60d54799e56bcb7e727a1c5c94f19bf
Ovi Savescu <ovi@ovswebsites.com>
2021-08-24 11:01:18 +0300

    fix: updated from BackgroundImage

--- src/components/services-area/index.js
+++ src/components/services-area/index.js
@@ -1,54 +1,62 @@
-import React from 'react'
-import {Link, useStaticQuery, graphql} from 'gatsby'
-import BackgroundImage from 'gatsby-background-image'
+import React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
 
-import Service from './service'
+import { getImage } from "gatsby-plugin-image";
+import { BgImage } from "gbimage-bridge";
 
-const ServicesArea = ({sectionTitle = 'Missing Title', sectionSubtitle, cardsContent}) => {
-  const {sectionBg} = useStaticQuery(
+import Service from "./service";
+
+const ServicesArea = ({ sectionTitle = "Missing Title", sectionSubtitle, cardsContent }) => {
+  const { sectionBg } = useStaticQuery(
     graphql`
-    query {
-      sectionBg: file(relativePath: { eq: "bg/bg-shape.jpg" }) {
-        childImageSharp {
-          fluid(quality: 90, maxWidth: 1920) {
-            ...GatsbyImageSharpFluid_withWebp_noBase64
+      query {
+        sectionBg: file(relativePath: { eq: "bg/bg-shape.jpg" }) {
+          childImageSharp {
+            gatsbyImageData(width: 1400, quality: 80)
           }
         }
       }
-    }
     `
-  )
+  );
+
+  const servicesAreaBgImage = getImage(sectionBg);
+
   return (
-    <BackgroundImage
-      Tag='div'
-      className='services-area in-section section-padding-lg'
-      fluid={sectionBg.childImageSharp.fluid}
+    <BgImage
+      Tag="div"
+      className="services-area in-section section-padding-lg"
+      image={servicesAreaBgImage}
       backgroundColor={`#FFFFFF`}
       fadeIn={false}
     >
-      <div className='container'>
-        <div className='row justify-content-center'>
-          <div className='col-lg-6'>
-            <div className='section-title text-center'>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-6">
+            <div className="section-title text-center">
               {sectionSubtitle && <h6>{sectionSubtitle}</h6>}
               <h2>{sectionTitle}</h2>
               <p>With a VA Home Loan Specialist, the process just got easier!</p>
             </div>
           </div>
         </div>
-        <div className='row'>
-
-          {cardsContent.map((serviceProps, index) => <Service key={index} {...serviceProps} />)}
-
+        <div className="row">
+          {cardsContent.map((serviceProps, index) => (
+            <Service key={index} {...serviceProps} />
+          ))}
         </div>
 
-        <div className='row justify-content-center'>
-          <Link to='/obtaining-a-va-home-loan/' type='submit' className='in-button in-button-theme mt-5'>Learn More</Link>
+        <div className="row justify-content-center">
+          <Link
+            to="/obtaining-a-va-home-loan/"
+            type="submit"
+            className="in-button in-button-theme mt-5"
+          >
+            Learn More
+          </Link>
         </div>
-
       </div>
-    </BackgroundImage>
-  )
-}
+    </BgImage>
+  );
+};
 
-export default ServicesArea
+export default ServicesArea;

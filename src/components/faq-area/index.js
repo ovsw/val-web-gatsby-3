e91feb80451c3d6c2d6847a1d82252bcdaf4a56b
Ovi Savescu <ovi@ovswebsites.com>
2021-08-24 10:53:19 +0300

    fix: updated BackgroundImage

--- src/components/faq-area/index.js
+++ src/components/faq-area/index.js
@@ -1,57 +1,59 @@
-import React from 'react'
-import {useStaticQuery, graphql} from 'gatsby'
-import BackgroundImage from 'gatsby-background-image'
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
-import QA from './qa'
+import { getImage } from "gatsby-plugin-image";
+import { BgImage } from "gbimage-bridge";
 
-const FAQArea = ({sectionTitle, sectionSubtitle, faqContent}) => {
-  const {sectionBg} = useStaticQuery(
+import QA from "./qa";
+
+const FAQArea = ({ sectionTitle, sectionSubtitle, faqContent }) => {
+  const { sectionBg } = useStaticQuery(
     graphql`
-    query {
-      sectionBg: file(relativePath: { eq: "bg/background-image-1.jpg" }) {
-        childImageSharp {
-          fluid(quality: 90, maxWidth: 1920) {
-            ...GatsbyImageSharpFluid_withWebp_noBase64
+      query {
+        sectionBg: file(relativePath: { eq: "bg/background-image-1.jpg" }) {
+          childImageSharp {
+            gatsbyImageData(width: 1200, quality: 90)
           }
         }
       }
-    }
     `
-  )
+  );
 
-  const backgroundFluidImageStack = [
-    sectionBg.childImageSharp.fluid,
-    `linear-gradient(rgba(3, 27, 56, 0.9), rgba(3, 27, 56, 0.9))`
-  ].reverse()
+  const faqSectionBgImage = getImage(sectionBg);
+
+  const backgroundImageStack = [
+    faqSectionBgImage,
+    `linear-gradient(rgba(3, 27, 56, 0.9), rgba(3, 27, 56, 0.9))`,
+  ].reverse();
 
   return (
-    <BackgroundImage
-      Tag='div'
-      className='calltoaction-area in-section section-padding-lg dark'
-      fluid={backgroundFluidImageStack}
+    <BgImage
+      Tag="div"
+      className="calltoaction-area in-section section-padding-lg dark"
+      image={backgroundImageStack}
       backgroundColor={`#163963`}
       fadeIn={false}
     >
-
-      <div className='container'>
-        <div className='row justify-content-center in-cta'>
-          <div className='col-lg-8' >
-            <div className='section-title text-center'>
+      <div className="container">
+        <div className="row justify-content-center in-cta">
+          <div className="col-lg-8">
+            <div className="section-title text-center">
               {sectionSubtitle && <h6>{sectionSubtitle}</h6>}
               <h2>{sectionTitle}</h2>
             </div>
           </div>
         </div>
-        <div className='row'>
-          <div className='col-lg-12'>
-            <div className='faequently-accordion'>
-              {faqContent && faqContent.map((QandA, index) => <QA key={index} item={index + 1} {...QandA} />)}
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="faequently-accordion">
+              {faqContent &&
+                faqContent.map((QandA, index) => <QA key={index} item={index + 1} {...QandA} />)}
             </div>
           </div>
         </div>
       </div>
-    </BackgroundImage>
-  )
-}
+    </BgImage>
+  );
+};
 
-export default FAQArea
+export default FAQArea;

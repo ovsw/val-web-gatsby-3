9f421566fdc3d8229dedda87bf13b59dd3ab2fbb
Ovi Savescu <ovi@ovswebsites.com>
2021-11-02 11:57:12 +0200

    added static apply page with no sidebar

---
+++ src/pages/apply.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { graphql } from "gatsby";
+import {
+  mapEdgesToNodes,
+  filterOutDocsWithoutSlugs,
+  filterOutDocsPublishedInTheFuture,
+} from "../lib/helpers";
+import Container from '../components/container'
+import GraphQLErrorList from "../components/graphql-error-list";
+
+import SEO from "../components/seo";
+import Layout from "../containers/layout";
+
+const ApplyPage = (props) => {
+
+  return (
+    <Layout noheader>
+      <SEO title="Apply for a VA Loan | VA Loan Specialist | VA Loans for Vets" description="Apply for a VA Loan. Complete our comprehensive VA Loan application that will provide us detailed information to speed up the processing of mortgage applications." />
+      <Container>
+      <div class="blog-content"><div><h2>Start the Process – Refinance or Apply for a New VA Loan</h2><p>By clicking "Start Application" below, you will be directed to a new site, affiliated with Jimmy Vercellino and Goldwater Bank, that will allow us to begin the mortgage loan application process.</p><p><a href="https://goldwaterbank.mymortgage-online.com/?loanapp&amp;siteid=4884527613&amp;lar=wstar_jvercellin&amp;workFlowId=29714" rel="noopener noreferrer" target="_blank" class="in-button in-button-theme"><span>Start Application</span></a></p></div><div class="in-blog-metabottom mt-30"></div></div>
+
+      </Container>
+    </Layout>
+  );
+};
+
+export default ApplyPage;

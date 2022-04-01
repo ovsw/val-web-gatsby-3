199e2764f47476084ed257ec9917cb7eff0ce066
OVIS <ovi@ovswebsites.com>
2022-04-01 19:39:37 +0300

    404 page small style improvements

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,12 +6,14 @@ import SEO from "../components/seo";
 const NotFoundPage = () => (
   <Layout>
     <div className="kontainer">
-      <div style={{ paddingTop: "6rem", paddingBottom: "6rem" }}>
+      <div style={{ paddingTop: "6rem", paddingBottom: "6rem" }} className="space-y-10">
         <SEO title="404: Not found" />
-        <h1>404 Error: Page not found</h1>
+        <h1 className="text-5xl">404 Error: Page not found</h1>
         <p>We're sorry, we can't find the page you're looking for...</p>
         <p>
-          <a href="/">Go to the home page</a>
+          <a href="/" className="underline text-primary">
+            Go to the home page?
+          </a>
         </p>
       </div>
     </div>

fb48eefbe412d4b598389ef523fae0ad7ccc4b44
OVIS <ovi@ovswebsites.com>
2022-04-14 15:25:02 +0300

    switched from gatsby-plugin-google-analytics to gatsby-plugin-google-gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -359,14 +359,41 @@ module.exports = {
     //     addUncaughtPages: true // optional: will fill up pages that are not caught by queries and mapping and list them under `sitemap-pages.xml`
     //   }
     // },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: config.googleAnalyticsID,
+    //     // Defines where to place the tracking script - `true` in the head and `false` in the body
+    //     head: true,
+    //     // sampleRate: 5,
+    //     // siteSpeedSampleRate: 10
+    //   },
+    // },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: config.googleAnalyticsID,
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // sampleRate: 5,
-        // siteSpeedSampleRate: 10
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          config.googleAnalyticsID, // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          // optimize_id: "OPT_CONTAINER_ID",
+          // anonymize_ip: true,
+          // cookie_expires: 0,
+          send_page_view: true,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: false,
+          send_page_view: true,
+          // Avoids sending pageview hits from custom paths
+          // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
       },
     },
     "gatsby-plugin-netlify",

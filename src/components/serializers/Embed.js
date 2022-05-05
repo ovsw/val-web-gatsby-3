b7b967a6143e1820f1203e8f96cb39c9f1e9fafa
OVIS <ovi@ovswebsites.com>
2022-05-05 18:53:36 +0300

    added old style embed for cognito forms that doesn't use iframe, for Funding Fee calculator and SEO reasons

--- src/components/serializers/Embed.js
+++ src/components/serializers/Embed.js
@@ -1,3 +1,24 @@
 import React from "react";
+import { useEffect } from "react";
+import Helmet from "react-helmet";
 
-export default ({ node }) => <div dangerouslySetInnerHTML={{ __html: node.code }} />;
+export default ({ node }) => {
+  useEffect(() => {
+    if (typeof window != undefined) {
+      window.Cognito.load("forms", { id: `${node.cognitoFormId}` });
+    }
+  });
+
+  if (node.cognitoForm) {
+    return (
+      <>
+        <Helmet>
+          <script src="https://www.cognitoforms.com/s/xPoircp7HEGoNo_tpFEirg"></script>
+        </Helmet>
+        <div class="cognito"></div>
+      </>
+    );
+  } else {
+    return <div dangerouslySetInnerHTML={{ __html: node.code }} />;
+  }
+};
